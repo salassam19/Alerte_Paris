e478a6ec0ca31a98b2a68d238dbe784f761051fc
salassam19 <salim10_4@msn.com>
2017-07-09 22:12:57 +0200

    widget modifier pour le twitter Alerte_Paris

--- Mon_Test/corps_indx.php
+++ Mon_Test/corps_indx.php
@@ -1,11 +1,11 @@
 <div class="row">
-	<div class="col-md-2">
-		<a class="twitter-timeline" href="https://twitter.com/kinglim18">Tweets by kinglim18</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+	<div class="col-md-2 sidebar_gauche">
+		<a class="twitter-timeline" href="https://twitter.com/AlerteParis">Tweets by AlerteParis</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
 	</div>
-	<div class="col-md-7">
+	<div class="col-md-7 main">
 		<?php include('presentation.php') ?>
 	</div>
-	<div class="col-md-3">
+	<div class="col-md-3 sidebar_droite">
 		<p>jkcansj jdas has ausd acdlas bhgvd gsa cgab asja sxhax acshh cagjhcay</p>
 	</div>
 </div>
