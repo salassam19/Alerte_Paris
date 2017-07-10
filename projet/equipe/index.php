87c8c0557d757c2aab8b090cece7883515b68c5b
Beronik <Beronik>
2017-07-10 12:33:59 +0200

    changement d'un truc ?

--- projet/equipe/index.php
+++ projet/equipe/index.php
@@ -11,55 +11,48 @@
 	<link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
 
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
 
-	<script type="text/javascript" src="https://code.jquery.com/
-	jquery-3.2.1.min.js"></script>
-	<script type="text/javascript" src="bootstrap/js/bootstrap.js"> </script>
-  	
-		 <title>Alerte Paris</title>
+	<title>Alerte Paris</title>
 </head>
 
 <body>
 
-<!--Barre de navegation et logo  -->
-<header class="cd-auto-hide-header">
+	<!--Barre de navegation et logo  -->
 	<header class="cd-auto-hide-header">
 		<?php include('logo.php') ?>
 
 		<!-- .cd-primary-nav -->
 		<?php include('formulaire_login_p1.php') ?>
 	</header>
-</header>
+	<section class="cd-hero">
+		<div class="cd-hero-content">
+			<!-- your content here -->
+		</div>
+	</section> <!-- .cd-hero -->
 
-<section class="cd-hero">
-	<div class="cd-hero-content">
-		<!-- your content here -->
-	</div>
-</section> <!-- .cd-hero -->
+	<nav class="cd-secondary-nav">
+		<?php include('nav2.php') ?>
+	</nav> <!-- .cd-secondary-nav -->
 
-<nav class="cd-secondary-nav">
-	<?php include('nav2.php') ?>
-</nav> <!-- .cd-secondary-nav -->
+	<main class="cd-main-content sub-nav-hero">
+		<div>
+			<?php include('corps_indx.php') ?>
+		</div>
+	</main> 
 
-<main class="cd-main-content sub-nav-hero">
-	<div>
-		<?php include('corps_indx.php') ?>
-	</div>
-</main> 
+	<footer>
+	</footer>
 
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+	<script>
+		if( !window.jQuery ) document.write('<script src="js/jquery-3.0.0.min.js"><\/script>');
+	</script>
 
-	<footer></footer>
+	<script src="js/main.js"></script>
 
-
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
-<script>
-	if( !window.jQuery ) document.write('<script src="js/jquery-3.0.0.min.js"><\/script>');
-</script>
-
-<script src="js/main.js"></script>
-
-
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="bootstrap/js/bootstrap.js"> </script>
 
 </body>
 </html>
--- projet/equipe/nav2.php
+++ projet/equipe/nav2.php
@@ -1,11 +1,9 @@
-<nav class="cd-secondary-nav">
-	<ul>
-		<li><a href="nous.php">Qui somme nous</a></li>
-		<li><a href="Info_Important.php">Info important</a></li>
-		<li><a href="Carte.php">Cartes</a></li>
-		<li><a href="Archive.php">Archives</a></li>
-		<li><a href="Image.php">Photos</a></li>
-		<li><a href="Blog.php">Blog</a></li>
-		<li><a href="#Contacte">Contact</a></li>
-	</ul>
-</nav>
+<ul>
+	<li><a href="nous.php">Qui somme nous</a></li>
+	<li><a href="Info_Important.php">Info important</a></li>
+	<li><a href="Carte.php">Cartes</a></li>
+	<li><a href="Archive.php">Archives</a></li>
+	<li><a href="Image.php">Photos</a></li>
+	<li><a href="Blog.php">Blog</a></li>
+	<li><a href="#Contacte">Contact</a></li>
+</ul>
