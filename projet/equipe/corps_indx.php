2533164100d3abbe5b928a0d7d3974adcc6d9e2d
Beronik <Beronik>
2017-07-10 11:27:43 +0200

    changement de col-md

--- projet/equipe/corps_indx.php
+++ projet/equipe/corps_indx.php
@@ -2,10 +2,10 @@
 	<div class="col-md-2 sidebar_gauche">
 		<a class="twitter-timeline" href="https://twitter.com/AlerteParis">Tweets by AlerteParis</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
 	</div>
-	<div class="col-md-7 main">
+	<div class="col-md-4 main">
 		<?php include('presentation.php') ?>
 	</div>
-	<div class="col-md-3 sidebar_droite">
+	<div class="col-md-6 sidebar_droite">
 		<iframe src="https://www.google.com/maps/d/u/0/embed?mid=1zAHgpTqKBW_OgeY8pPh6_f41ge0" width="640" height="480"></iframe>
 	</div>
 </div>
