1a50f876797091c9809e2495a18c1cdf101459e9
AURORE TRINCI <aurore.trinci@gmail.com>
2017-07-12 15:59:03 +0200

    formulaire V4

--- functions/fr/functions_signin_fr.php
+++ functions/fr/functions_signin_fr.php
@@ -1,40 +1,62 @@
 <?php
+	
+	// on déclare nos variables
+		$pseudo 			= $_POST['pseudo'];
+		$last_name 			= $_POST['last_name'];
+		$first_name 		= $_POST['first_name'];
+		$email 				= $_POST['email'];
+		$password 			= $_POST['password'];
+		$conf_password		= $_POST['conf_password'];
+		$birthday 			= $_POST['birthday'];
+		$gender 			= $_POST['gender'];
+		$address 			= $_POST['address'];
+		$post_code 			= $_POST['post_code'];
+		$city 				= $_POST['city'];
+		$country 			= $_POST['country'];
+		$mobile_phone 		= $_POST['mobile_phone'];
+		$home_phone 		= $_POST['home_phone'];
+		$host 				= $_POST['host'];
+		$choice_form		= $_POST['choice_form'];
 
-	include("../functions_bdd.php")
-	
-	// on teste si nos champs sont remplis
-	foreach ($_POST as $key => $value){
-		if (!empty($value)) {
+		// on teste si nos champs sont remplis
+		if (!empty($last_name) && !empty($first_name) && !empty($email) && !empty($password) && !empty($conf_password) && !empty($birthday) && !empty($gender) && !empty($address) && !empty($post_code) && !empty($city) && !empty($country) && !empty($mobile_phone) && !empty($home_phone) && !empty($host) && !empty($choice_form)) {
 			// nos champs sont remplis
-			$email = $_POST['email'];
-				if(preg_match('#^[\w.-]+@[\w.-]+\.[a-z]{2,6}$#i', $email)){
-					// email valide
-					if ($key === "password") {
-					$password = $_POST['password'];
-						if (preg_match('#^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}#', $password)) {
-							$conf_password = $_POST["conf_password"];
-							if ($password == $conf_password) {
-				} else {
-					// email invalide
-					echo("L'adresse email est non valide.");
-				}
-					} else {
-						echo('Votre mot de passe doit contenir au moins 8 caractères, 1 majuscule, 1 minuscule et 1 chiffre.');
-					}
-						} else {
-							echo('Les 2 mots de passe ne sont pas identiques.');
-						}
-				}
+			if(preg_match('#^[\w.-]+@[\w.-]+\.[a-z]{2,6}$#i', $email)){
+				// email valide
+				?><script language="javascript"> console.log("L'email est ok.");</script><?php
+			} else {
+				// email invalide
+				?><script language="javascript"> alert("L'adresse email n'est pas valide.);</script><?php
+			}
+			// on impose un password contenant au moins 8 caractères, 1 majuscule, 1 minuscule et 1 chiffre
+			if (preg_match('#^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}#', $password)) {
+				// mot de passe valide
+				?><script language="javascript"> console.log("Le mot de passe est ok.");</script><?php
+			} else {
+				// mot de passe non conforme
+				?><script language="javascript"> alert("Votre mot de passe doit contenir au moins 8 caractère, une majuscule, une minuscule et 1 chiffre.");</script><?php
+			}
+			// on vérifie que les 2 mots de passe sont identiques
+			if ($password == $conf_password) {
+				// les 2 mots de passe sont identiques
+				?><script language="javascript"> console.log("Les 2 mots de passe sont identiques.");</script><?php
+			}else{
+				// les 2 mots de passe ne sont pas identiques
+				?><script language="javascript"> alert("Les 2 mots de passe ne sont pas identiques.");</script><?php
+			}
+		}else {
+			header('Location:../../views/fr/signin_fr.php');
 		}
-	}
 	
+	
+
+	include("../functions_bdd.php");
 	// on appelle notre fonction de connexion à la bdd
 	connexion_bdd();
 
 	// on appelle notre fonction pour l'enregistrement des données du formulaire signin dans la bdd
 	signin_register();
 
-							
 	// on teste si il y a bien une checkbox sur le host et le choice_form
 	if (isset($_POST['host']) AND isset($_POST['choice_form'])) {
 		// si l'utilisateur répond qu'il est un particulier et qu'il souhaite accueillir des gens, on inclus le formulaire particuliers
@@ -53,7 +75,8 @@
 		if (($_POST['host'] === 'yes') AND ($_POST['choice_form']) === 'professional') {
 			include("../../views/fr/part_form_fr.php");
 		}
-	}
+	}			
+	
 
 
 
--- functions/functions_bdd.php
+++ functions/functions_bdd.php
@@ -2,27 +2,49 @@
 
 	// Connexion à la bdd
 	function connexion_bdd() {
-		$bdd = $bdd = new PDO('mysql:host=localhost;dbname=alerte_paris;"root", ""');
+		$bdd = new PDO('mysql:host=localhost;dbname=alerte_paris;charset=utf8', 'root', '');
+		return $bdd;
 	}
 
 	// Enregistrement des données dans la bdd à partir du formulaire signin
-	function signin_register() {}
-	$req = $bdd->prepare('INSERT INTO auteur (aut_pseudo, aut_nom, aut_prenom, aut_email, aut_password, aut_birthday, aut_sexe, aut_adresse, aut_code_postal, aut_ville, aut_pays, aut_mobile, aut_fix, aut_hebergeur, aut_pro) VALUES (:pseudo, :last_name, :first_name, :email, :password, :birthday, :gender, :address, :post_code, :city, :country, :mobile_phone, :home_phone, :host, :choice_form)');
+	function signin_register() {
+	// on déclare les variables pour indiquer où aller chercher les valeurs
+		$pseudo 			= $_POST['pseudo'];
+		$last_name 			= $_POST['last_name'];
+		$first_name 		= $_POST['first_name'];
+		$email 				= $_POST['email'];
+		$password 			= $_POST['password'];
+		$birthday 			= $_POST['birthday'];
+		$gender 			= $_POST['gender'];
+		$address 			= $_POST['address'];
+		$post_code 			= $_POST['post_code'];
+		$city 				= $_POST['city'];
+		$country 			= $_POST['country'];
+		$mobile_phone 		= $_POST['mobile_phone'];
+		$home_phone 		= $_POST['home_phone'];
+		$host 				= $_POST['host'];
+		$choice_form		= $_POST['choice_form'];
 
-	$req->execute(array(
-		'aut_pseudo' 		=> $_POST['pseudo'];
-		'aut_nom' 			=> $_POST['last_name'];
-		'aut_prenom' 		=> $_POST['first_name'];
-		'aut_email' 		=> $_POST['email'];
-		'aut_password'		=> $_POST['password'];
-		'aut_birthday' 		=> $_POST['birthday'];
-		'aut_sexe'			=> $_POST['gender'];
-		'aut_adresse' 		=> $_POST['address'];
-		'aut_code_postal'	=> $_POST['post_code'];
-		'aut_ville' 		=> $_POST['city'];
-		'aut_pays' 			=> $_POST['country'];
-		'aut_mobile' 		=> $_POST['mobile_phone'];
-		'aut_fix' 			=> $_POST['home_phone'];
-		'aut_hebergeur' 	=> $_POST['host'];
-		'aut_pro' 			=> $_POST['choice_form'];
-	));
+		$bdd = new PDO('mysql:host=localhost;dbname=alerte_paris;charset=utf8', 'root', '');
+		$req = $bdd->prepare('INSERT INTO "auteur" ("aut_pseudo", "aut_nom", "aut_prenom", "aut_email", "aut_password", "aut_birthday", "aut_sexe", "aut_adresse", "aut_code_postal", "aut_ville", "aut_pays", "aut_mobile", "aut_fix", "aut_hebergeur", "aut_pro") VALUES ($pseudo, $last_name, $first_name, $email, $password, $birthday, $gender, $address, $post_code, $city, $country, $mobile_phone, $home_phone, $host, $choice_form)');
+
+
+		$req->execute(array(
+			'aut_pseudo' 		=> $pseudo,
+			'aut_nom' 			=> $last_name,
+			'aut_prenom' 		=> $first_name,
+			'aut_email' 		=> $email,
+			'aut_password'		=> $password,
+			'aut_birthday' 		=> $birthday,
+			'aut_sexe'			=> $gender,
+			'aut_adresse' 		=> $address,
+			'aut_code_postal'	=> $post_code,
+			'aut_ville' 		=> $city,
+			'aut_pays' 			=> $country,
+			'aut_mobile' 		=> $mobile_phone,
+			'aut_fix' 			=> $home_phone,
+			'aut_hebergeur' 	=> $host,
+			'aut_pro' 			=> $choice_form
+		));
+	
+	}
